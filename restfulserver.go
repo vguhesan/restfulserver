f2f6b20ebcc1b8036a9bbf9ae0c4687fcf53c0e5
Venkatt Guhesan <vguhesan@gmail.com>
2021-02-18 17:58:36 -0500

    Simple listener with a root (/) end-point with a "hello world".

--- restfulserver.go
+++ restfulserver.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "Welcome to the HomePage!")
+    fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests() {
+    http.HandleFunc("/", homePage)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
 func main() {
-	println("Ba dum, tss!")
+    println("Server listening on port 8080")
+	handleRequests()
 }
