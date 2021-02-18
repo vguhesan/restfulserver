84af230638519c0dc0597b0d973ab41e9825c3ce
Venkatt Guhesan <vguhesan@gmail.com>
2021-02-18 18:34:23 -0500

    /hello end-point implemented using raw http interfaces.

--- restfulserver.go
+++ restfulserver.go
@@ -6,17 +6,25 @@ import (
 	"net/http"
 )
 
+
 func homePage(w http.ResponseWriter, r *http.Request){
     fmt.Fprintf(w, "Welcome to the HomePage!")
     fmt.Println("Endpoint Hit: homePage")
 }
 
+func helloEndpoint(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Endpoint Hit: hello")
+	message := "{\"hello\"}"
+	fmt.Fprintf(w, message)
+}
+
 func handleRequests() {
     http.HandleFunc("/", homePage)
+	http.HandleFunc("/hello", helloEndpoint)
     log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("Server listening on port 8080")
 }
 
 func main() {
-    println("Server listening on port 8080")
 	handleRequests()
 }
