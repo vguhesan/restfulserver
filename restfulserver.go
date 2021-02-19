d8918ac5ed388bdbc71e2e88443de2ef6c83a16d
Venkatt Guhesan <vguhesan@gmail.com>
2021-02-19 12:39:21 -0500

    Implemented concat handler and the supporting unit test.

--- restfulserver.go
+++ restfulserver.go
@@ -1,9 +1,11 @@
 package restfulserver
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -16,21 +18,47 @@ type ServerHook struct {
 
 var srvHook ServerHook
 
+// Default root handler
 func homeEndpoint(w http.ResponseWriter, r *http.Request){
     fmt.Fprintf(w, "Welcome to the HomePage!")
-    fmt.Println("Endpoint Hit: homePage")
+    fmt.Println("Endpoint Hit: home")
 }
 
+// Hello handler
 func helloEndpoint(w http.ResponseWriter, r *http.Request){
 	fmt.Println("Endpoint Hit: hello")
 	message := "{\"hello\"}"
 	fmt.Fprintf(w, message)
 }
 
+// Concatenate a set of strings
+type concatenate struct {
+	List []string `json:"List"`
+}
+
+type catmsg struct {
+    Result string `json:"Result"`
+}
+
+// Concat handler
+func concatEndpoint(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Endpoint Hit: concat")
+	var cat concatenate
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&cat); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+        return
+    }
+    defer r.Body.Close()
+	result := catmsg{ Result : strings.Join(cat.List,"")}
+	respondWithJSON(w, http.StatusCreated, result)
+}
+
 func handleRequests() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeEndpoint)
 	router.HandleFunc("/hello", helloEndpoint)
+	router.HandleFunc("/concat", concatEndpoint).Methods("POST")
 	fmt.Println("Server listening on port 8080")
 	srvHook.srv = &http.Server{
         Handler:      router,
@@ -39,6 +67,18 @@ func handleRequests() {
         ReadTimeout:  3 * time.Second,
     }
 	log.Fatal(srvHook.srv.ListenAndServe())
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+    respondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+    response, _ := json.Marshal(payload)
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(code)
+    w.Write(response)
 }
 
 func init() {
--- restfulserver_test.go
+++ restfulserver_test.go
@@ -1,6 +1,8 @@
 package restfulserver
 
 import (
+	"bytes"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"testing"
@@ -27,5 +29,37 @@ func TestHelloEndPoint(t *testing.T) {
 		panic(err)
 	}
 	assert.Equal(t, []byte("{\"hello\"}"), body)
+}
+
+func TestConcatEndPoint(t *testing.T) {
+	client := &http.Client{
+		Timeout: 3 * time.Second,
+	}
+	var jsonStr = []byte(
+		`
+		{
+			"List": [
+			"abcd",
+			"egh",
+			"ijkmnop"
+			]
+		}	
+		`)
+	r, _ := http.NewRequest("POST", srvURL+"/concat", bytes.NewBuffer(jsonStr))
+	r.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(r)
+	if err != nil {
+		panic(err)
+	}
+	assert.Equal(t, http.StatusCreated, resp.StatusCode)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	var m map[string]interface{}
+	json.Unmarshal(body, &m)
+	var expectedValue = "abcdeghijkmnop"
+	var actualValue = m["Result"]
+	assert.Equal(t, expectedValue, actualValue)
 }
 
